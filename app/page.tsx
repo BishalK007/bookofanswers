cd5770e82696f96256664d7170d1d9f643357481
Bishal <bishalkarmakar007@gmail.com>
2024-06-24 02:45:29 +0530

    FIXING

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,16 @@ const Page = () => {
       setIsDrawing(true);
     }
     else {
-      const response = await fetch('/api/getrandomquote');
+      const timestamp = new Date().toISOString(); // Example of creating a timestamp
+      const response = await fetch('/api/getrandomquote', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ timestamp })
+      });
       const data = await response.json();
+
       setResponsequote(data.quote);
       setIsDrawing(false);
     }
@@ -25,19 +33,19 @@ const Page = () => {
 
   return (
     <div className='gradient-background w-screen h-[100dvh] font-medival flex flex-col px-10 text-center content-center'>
-      <div className='flex flex-row justify-center text-5xl font-bold text-customBrown pt-10'>Book To Answers 
-      <span
-        className="text-xl cursor-pointer"
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        onClick={() => setHover(!hover)}
-      >
-       &#9432;
-      </span>
+      <div className='flex flex-row justify-center text-5xl font-bold text-customBrown pt-10'>Book To Answers
+        <span
+          className="text-xl cursor-pointer"
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          onClick={() => setHover(!hover)}
+        >
+          &#9432;
+        </span>
         <div className={cn("absolute right-0 w-32 bg-customBrown text-white text-sm rounded shadow-lg z-20 translate-y-[-100px]  opacity-0 transition-all duration-700", hover && ' translate-y-10 opacity-1')}>
           Hope you realize it&apos;s a JOKE &#33;&#33;
         </div>
-      </div> 
+      </div>
       <div className='w-full flex-grow flex flex-col justify-evenly items-center content-evenly text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
         <div className="relative w-full">
           <div className={cn('transition-opacity duration-500 absolute w-full', isDrawing ? 'opacity-0' : 'opacity-1')}>
@@ -50,7 +58,7 @@ const Page = () => {
 
         <div className="relative w-full">
           <div className={cn('transition-opacity duration-500 absolute w-full', isDrawing ? 'opacity-0' : 'opacity-1')}>
-            <div>{<div className={cn('text-2xl transition-opacity delay-500', isFirst ? 'opacity-0' : 'opacity-1')}>Previous Answer -</div>}<br/>{responsequote}</div>
+            <div>{<div className={cn('text-2xl transition-opacity delay-500', isFirst ? 'opacity-0' : 'opacity-1')}>Previous Answer -</div>}<br />{responsequote}</div>
           </div>
           <div className={cn('transition-opacity duration-500 absolute  flex flex-row justify-center items-center w-full', isDrawing ? 'opacity-1' : 'opacity-0')}>
             <div className="animate-bounce w-fit">
